9c2da17a7781e22bd0fa20512dbae0e6f3221f09
Abhishek kumar singh <abhisheksinghbhtr967@gmail.com>
2024-11-30 15:25:24 +0530

    adding the print statement

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 
 func main() {
 	fmt.Print("Hello world from the abhishek aur beta ")
+
 	// fmt.Println("Learn my hello world ")
 	myutil.PrintMessage("Hello from the util")
+	fmt.Print("hi after the learning ")
+	// myabhishek.Mrintaessage("Hello")
+	// myabhishek.Mrintaessage("Hello fomr the abhishek")
 }
