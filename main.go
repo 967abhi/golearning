5d850486c521b01da52133ad5ba401f20f507ae0
Abhishek kumar singh <abhisheksinghbhtr967@gmail.com>
2024-11-30 14:59:54 +0530

    adding util folder

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"mylearning/myutil"
+)
 
 func main() {
 	fmt.Print("Hello world from the abhishek aur beta ")
+	// fmt.Println("Learn my hello world ")
+	myutil.PrintMessage("Hello from the util")
 }
